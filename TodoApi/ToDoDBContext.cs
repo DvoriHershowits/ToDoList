86983bd8c4aa3773aa813b740d5199f44f1f8ee1
DvoriHershowits <d0548432970@gmail.com>
2023-08-21 15:59:14 +0300

    add connection string

--- TodoApi/ToDoDBContext.cs
+++ TodoApi/ToDoDBContext.cs
@@ -18,7 +18,14 @@ public partial class ToDoDBContext : DbContext
     public virtual DbSet<Item> Items { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string connectionString = "MYSQL_ADDON_URI=mysql://udbq9sebjleqnttw:AlfRu4efirNKxgJPzeD1@bnyu2rpupoothabum3gz-mysql.services.clever-cloud.com:3306/bnyu2rpupoothabum3gz";
+            optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
+        }
+    }
+    //   => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
